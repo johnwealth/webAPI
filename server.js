38715afe24e763d9ec2bf356388c6508af5bab45
john wealth <johnwealth.ise@gmail.com>
2017-05-10 20:32:00 -0700

    mending hard coded port 2

--- server.js
+++ server.js
@@ -16,6 +16,6 @@ app.get('/content', function(req, res){
 app.use(express.static(__dirname + '/public'));
 
 
-http.listen(process.env.PORT || 3000, function(){
-  console.log('listening on', http.address().port);
+  app.listen(process.env.PORT || 3000, function(){
+  console.log('listening on port: ' + PORT);
 });
